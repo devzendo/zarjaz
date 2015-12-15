81c77586d018dce27e964bd6985f2e9c5001dbc8
Matt Gumbley <matt.gumbley@devzendo.org>
2015-12-15 08:05:55 +0000

    Sample validators; I'll come back to this.

---
+++ zarjaz-core/src/main/java/org/devzendo/zarjaz/validation/DefaultServerImplementationValidator.java
@@ -0,0 +1,27 @@
+package org.devzendo.zarjaz.validation;
+
+/**
+ * Copyright (C) 2008-2015 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class DefaultServerImplementationValidator implements ServerImplementationValidator {
+
+    public DefaultServerImplementationValidator() {
+    }
+
+    @Override
+    public <T> void validateServerImplementation(final Class<T> interfaceClass, final T implementation) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
