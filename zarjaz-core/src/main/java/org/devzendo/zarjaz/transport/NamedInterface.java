be4d1d043c305dff118a5a40cf087038505fbc2a
Matt Gumbley <matt.gumbley@devzendo.org>
2015-12-15 08:03:59 +0000

    NamedInterfaces are used internally by transports as keys into the server implementation map.

---
+++ zarjaz-core/src/main/java/org/devzendo/zarjaz/transport/NamedInterface.java
@@ -0,0 +1,64 @@
+package org.devzendo.zarjaz.transport;
+
+/**
+ * Copyright (C) 2008-2015 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class NamedInterface <T> {
+    private final EndpointName name;
+    private final Class<T> interfaceClass;
+
+    public NamedInterface(final EndpointName name, final Class<T> interfaceClass) {
+        this.name = name;
+        this.interfaceClass = interfaceClass;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!NamedInterface.class.isAssignableFrom(obj.getClass())) {
+            return false;
+        }
+        final NamedInterface other = (NamedInterface) obj;
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (interfaceClass == null) {
+            if (other.interfaceClass != null) {
+                return false;
+            }
+        } else if (!interfaceClass.equals(other.interfaceClass)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((interfaceClass == null) ? 0 : interfaceClass.hashCode());
+        return result;
+    }
+}
---
+++ zarjaz-core/src/test/java/org/devzendo/zarjaz/transport/TestNamedInterface.java
@@ -0,0 +1,64 @@
+package org.devzendo.zarjaz.transport;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * Copyright (C) 2008-2015 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class TestNamedInterface {
+    public interface Foo {
+
+    }
+
+    public interface Bar {
+
+    }
+
+    @Test
+    public void equalInterfaces() {
+        final NamedInterface v1 = new NamedInterface(new EndpointName("foo"), Foo.class);
+        final NamedInterface v2 = new NamedInterface(new EndpointName("foo"), Foo.class);
+        assertThat(v1.equals(v2), equalTo(true));
+        assertThat(v2.equals(v1), equalTo(true));
+    }
+
+    @Test
+    public void unequalInterfacesByName() {
+        final NamedInterface v1 = new NamedInterface(new EndpointName("foo"), Foo.class);
+        final NamedInterface v2 = new NamedInterface(new EndpointName("bar"), Foo.class);
+        assertThat(v1.equals(v2), equalTo(false));
+        assertThat(v2.equals(v1), equalTo(false));
+    }
+
+    @Test
+    public void unequalInterfacesByInterface() {
+        final NamedInterface v1 = new NamedInterface(new EndpointName("xxx"), Foo.class);
+        final NamedInterface v2 = new NamedInterface(new EndpointName("xxx"), Bar.class);
+        assertThat(v1.equals(v2), equalTo(false));
+        assertThat(v2.equals(v1), equalTo(false));
+    }
+
+    @Test
+    public void unequalInterfacesByNameAndInterface() {
+        final NamedInterface v1 = new NamedInterface(new EndpointName("foo"), Foo.class);
+        final NamedInterface v2 = new NamedInterface(new EndpointName("bar"), Bar.class);
+        assertThat(v1.equals(v2), equalTo(false));
+        assertThat(v2.equals(v1), equalTo(false));
+    }
+
+}
