46da0d135fae8ae1ce16584a7faa4e56c09091c6
Matt Gumbley <matt.gumbley@devzendo.org>
2017-03-10 08:15:19 +0000

    More small refactoring in the NullTransceiver.

--- zarjaz-core/src/main/java/org/devzendo/zarjaz/transceiver/NullTransceiver.java
+++ zarjaz-core/src/main/java/org/devzendo/zarjaz/transceiver/NullTransceiver.java
@@ -59,7 +59,7 @@ public class NullTransceiver implements Transceiver {
             queue.add(() -> {
                 logger.debug("Dispatching queued ByteBuffer to " + sendToEndName + " end");
                 NullTransceiver.dumpBuffers(data);
-                sendToEnd.observers.eventOccurred(new DataReceived(data, replyWriter));
+                sendToEnd.fireEvent(new DataReceived(data, replyWriter));
             });
         }
     }
@@ -69,6 +69,10 @@ public class NullTransceiver implements Transceiver {
     private class NullObservableTransceiverEnd implements ObservableTransceiverEnd {
 
         private final ObserverList<TransceiverObservableEvent> observers = new ObserverList<>();
+
+        public void fireEvent(final TransceiverObservableEvent event) {
+            observers.eventOccurred(event);
+        }
 
         @Override
         public void addTransceiverObserver(final TransceiverObserver observer) {
